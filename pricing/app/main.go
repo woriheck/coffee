4a43cfb21f8d730359c3f4eda2c4720b36ceb719
woriheck <woriheck@gmail.com>
2022-05-29 19:43:06 +0800

    update

--- pricing/app/main.go
+++ pricing/app/main.go
@@ -3,29 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", HelloProduct)
-	http.HandleFunc("/pricing", HelloPricing)
+	http.HandleFunc("/", HelloPricing)
 	http.ListenAndServe(":8080", nil)
 }
 
 type Response struct {
-	Message string `json:"message"`
-}
-
-func HelloProduct(w http.ResponseWriter, r *http.Request) {
-	jsonOut, _ := json.Marshal(Response{Message: "Hello GUYS?"})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "%s", jsonOut)
+	Message         string      `json:"message"`
+	UpStreamMessage interface{} `json:"up_stream_message"`
 }
 
 func HelloPricing(w http.ResponseWriter, r *http.Request) {
-	jsonOut, _ := json.Marshal(Response{Message: "Hello Pricing?"})
+	resp := callJsonTest()
+	// json.Unmarshal(,)
+
+	var mp map[string]interface{}
+	err := json.Unmarshal(resp, &mp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	jsonOut, _ := json.Marshal(
+		Response{
+			Message:         "Pricing service",
+			UpStreamMessage: mp,
+		},
+	)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	fmt.Fprintf(w, "%s", jsonOut)
+}
+
+func callJsonTest() []byte {
+	resp, err := http.Get("http://time.jsontest.com")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return body
 }
--- product/app/main.go
+++ product/app/main.go
@@ -3,29 +3,50 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", HelloProduct)
-	http.HandleFunc("/pricing", HelloPricing)
 	http.ListenAndServe(":8080", nil)
 }
 
 type Response struct {
-	Message string `json:"message"`
+	Message         string      `json:"message"`
+	UpStreamMessage interface{} `json:"up_stream_message"`
 }
 
 func HelloProduct(w http.ResponseWriter, r *http.Request) {
-	jsonOut, _ := json.Marshal(Response{Message: "Hello GUYS?"})
+	resp := callPricingService()
+	var mp map[string]interface{}
+	err := json.Unmarshal(resp, &mp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	jsonOut, _ := json.Marshal(
+		Response{
+			Message:         "Product service",
+			UpStreamMessage: mp,
+		},
+	)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	fmt.Fprintf(w, "%s", jsonOut)
 }
 
-func HelloPricing(w http.ResponseWriter, r *http.Request) {
-	jsonOut, _ := json.Marshal(Response{Message: "Hello Pricing?"})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "%s", jsonOut)
+func callPricingService() []byte {
+	resp, err := http.Get(os.Getenv("PRICING_SVC_URI"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return body
 }
